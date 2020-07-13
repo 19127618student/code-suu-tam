03477f67d03f2904ba2cc58fa6d85dfd5d98d6bc
19127618student <63986381+19127618student@users.noreply.github.com>
2020-07-13 19:29:28 +0700

    Add files via upload

---
+++ codeSuuTam.cpp
@@ -0,0 +1,8 @@
+//doi d/m/y -> d 
+int thisIsMagic(int year, int month, int day) {
+	if (month < 3) {
+		year--;
+		month += 12;
+	}
+	return 365 * year + year / 4 - year / 100 + year / 400 + (153 * month - 457) / 5 + day - 306;
+}
